ff93e8cc3b081085c7f7eb22a49dd7f1036263e1
Ferid Kamoua <kamouaferid@gmail.com>
2023-03-15 22:44:17 -0700

    added cors to express

--- server/routes/scrapeRoutes.js
+++ server/routes/scrapeRoutes.js
@@ -6,6 +6,7 @@ const router = express();
 router.post("/scrape", async (req, res) => {
   const data = await scrape("software+developer", "East+Setauket", "NY", 1);
   console.log(data);
+  res.send(data);
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = 3000;
 
 const scrapeRouter = require("./routes/scrapeRoutes");
 
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSucessStatus: 200,
+};
+
 app.use(express.json());
+app.use(cors(corsOptions));
+
 app.use(scrapeRouter);
 
 app.listen(port, () => {
